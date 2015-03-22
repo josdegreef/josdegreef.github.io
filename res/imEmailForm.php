f8c1a838d403d7f2faea9760dcf1dd945f8d84ca
josdegreef <jos.degreef@telenet.be>
2015-03-22 14:33:29 +0100

    adapt loc x5engine.php

--- res/imEmailForm.php
+++ res/imEmailForm.php
@@ -1,6 +1,6 @@
 <?php
 if(substr(basename($_SERVER['PHP_SELF']), 0, 11) == "imEmailForm") {
-	include 'x5engine.php';
+	include '/res/x5engine.php';
 	$form = new ImForm();
 	$form->setField('Naam:', $_POST['imObjectForm_1_1'], '', false);
 	$form->setField('Adres:', $_POST['imObjectForm_1_2'], '', false);
